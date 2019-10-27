ab4d269410269bbf0cc1b7df9add960d2d25c024
shishao-home <shishaoqi0928@gmail.com>
2019-10-27 16:05:06 +0800

    新改版：拆分 webpack.config.js 文件 -- 在build文件夹中，base及client文件

---
+++ build/webpack.config.base.js
@@ -0,0 +1,64 @@
+const path = require('path')
+
+//高版本必加项
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+const config = {
+    //使生成的bundle.js不被压缩
+    mode: 'none',
+
+    //入口， __dirname 是当前文件所在目录
+    entry: path.join(__dirname, '../src/index.js'),
+
+    //输出
+    output: {
+        filename: 'bundle.[hash:8].js',
+        path: path.join(__dirname, '../dist')
+    },
+
+    //区分打包类库代码
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        },
+        runtimeChunk: true
+    },
+
+    plugins: [
+        // make sure to include the plugin for the magic
+        new VueLoaderPlugin()
+    ],
+    module: {
+        rules: [
+            {
+              test: /\.vue$/,
+              loader: 'vue-loader'
+            },
+            {
+              test: /\.jsx$/,
+              loader: 'babel-loader'
+            },
+            {
+              test: /\.js$/,
+              loader: 'babel-loader',
+              exclude: /node_modules/
+            },
+            //将小于1024d的图片转为base64，减少http请求
+            {
+                test: /\.(gif|jpg|jpeg|png|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 1024,
+                            name: 'resources/[path][name].[hash:8].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
+
+
+module.exports = config;
---
+++ build/webpack.config.client.js
@@ -0,0 +1,109 @@
+const path = require('path')
+
+const HTMLPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const ExtractPlugin = require('extract-text-webpack-plugin');
+const baseConfig = require('./webpack.config.base');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const defaultPlugins = [
+  new webpack.DefinePlugin({
+      'process.env': {
+          NODE_ENV: isDev ? '"development"' : '"production"'
+      }
+  }),
+  new HTMLPlugin()
+]
+
+const devServer = {
+  port: 8080,
+  host: '0.0.0.0',
+  overlay: {
+      errors: true,
+  },
+  //open: true
+  //把webpack不能解释的地址指向到首页
+  //historyFallback: {}
+  //
+  hot: true
+}
+
+let config
+
+// 由于加入webpack-dev-server，要应对不同环境变量
+if(isDev){
+  config = merge(baseConfig, {
+    entry: path.join(__dirname, '../src/index.js'),
+    output: {
+      //filename: '[name].[chunkhash:8].js', //生产环境 css、js文件单独打包
+      filename: isDev ? '[name].[hash:8].js' : '[name].[chunkHash:8].js', //生产环境 css、js文件单独打包
+      path: path.join(__dirname, 'dist')
+    },
+    //让浏览器显示正常代码
+    devtool: "#cheap-module-eval-source-map",
+    module: {
+      //开发环境 styl 的配置
+      rules: [
+        {
+          test: /\.styl/,
+          use: ExtractPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: true,
+                }
+              },
+              'stylus-loader'
+            ]
+          })
+        }
+      ]
+    },
+    devServer,
+    plugins: defaultPlugins.concat([
+      new ExtractPlugin('styles.[md5:contenthash:hex:8].css'),
+      new webpack.HotModuleReplacementPlugin()//针对 hot: true 加入的
+    ])
+  })
+}else{
+  config = merge(baseConfig, {
+    entry: path.join(__dirname, '../src/index.js'),
+    output: {
+      //filename: '[name].[chunkhash:8].js', //生产环境 css、js文件单独打包
+      filename: isDev ? '[name].[hash:8].js' : '[name].[chunkHash:8].js', //生产环境 css、js文件单独打包
+      path: path.join(__dirname, 'dist')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.styl/,
+          use: ExtractPlugin.extract({
+            fallback: 'style-loader',
+            use: [
+              'css-loader',
+              {
+                loader: 'postcss-loader',
+                options: {
+                  sourceMap: true,
+                }
+              },
+              'stylus-loader'
+            ]
+          })
+        }
+      ]
+    },
+    plugins: defaultPlugins.concat([
+      //new ExtractPlugin('styles.[contentHash:8].css')
+      new ExtractPlugin('styles.[md5:contenthash:hex:8].css')
+    ])
+  })
+}
+
+//加入webpack-dev-server后改变的
+module.exports = config;
